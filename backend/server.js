e6be3ac88520402e8d341def0d3725642e435115
Sihathun <sihathunsin@gmail.com>
2025-10-23 21:19:03 +0700

    make changes to server.js file

--- backend/server.js
+++ backend/server.js
@@ -64,13 +64,7 @@ app.use("/api/products", productRoutes);
 async function initDB() {
     try {
         await sql`
-            CREATE TABLE IF NOT EXISTS products (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(255) NOT NULL,
-                image VARCHAR(255) NOT NULL,
-                price DECIMAL(10, 2) NOT NULL,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )
+            
         `;
         console.log("Database initialized successfully")
     } catch (error) {
